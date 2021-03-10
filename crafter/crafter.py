da471cdbbf59cd4564df7de1e0481388ed2d9abc
Danijar Hafner <mail@danijar.com>
2021-03-10 01:04:58 -0500

    Add FPS benchmark.

--- crafter/crafter.py
+++ crafter/crafter.py
@@ -304,15 +304,20 @@ def test_map():
 
 
 def test_episode():
+  import time
   env = Env(area=(64, 64), view=4, size=64, seed=17)
+  start = time.time()
   env.reset()
+  print(f'Reset time: {time.time()-start:.2f}s')
   frames = []
   random = np.random.RandomState(0)
+  start = time.time()
   for index in range(100):
     action = random.randint(0, env.action_space.n)
-    env.step(action)
-    image = env.render()
-    frames.append(image)
+    obs, _, _, _ = env.step(action)
+    frames.append(obs['image'])
+  duration = time.time() - start
+  print(f'Step time: {duration:.2f}s ({int(100/duration)} FPS)')
   cols = int(np.sqrt(len(frames)))
   rows = int(np.ceil(len(frames) / cols))
   grid = np.concatenate([
