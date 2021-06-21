d1a64872f02105d89c58c6dc53c918a8742ccf20
Danijar Hafner <mail@danijar.com>
2021-06-21 15:14:41 -0400

    Increase version number.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import pathlib
 
 setuptools.setup(
     name='crafter',
-    version='0.24.3',
+    version='0.25.0',
     description='Open world survival game for reinforcement learning.',
     url='http://github.com/danijar/crafter',
     long_description=pathlib.Path('README.md').read_text(),
