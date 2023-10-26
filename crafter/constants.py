b911159e07ae8ec3255abc3302032b388e0a8e9b
Simon Boehm <simon@siboehm.com>
2023-10-26 13:22:20 -0700

    Use new YAML API (for ruamel.yaml >= 0.18)

--- crafter/constants.py
+++ crafter/constants.py
@@ -3,5 +3,6 @@ import pathlib
 import ruamel.yaml as yaml
 
 root = pathlib.Path(__file__).parent
-for key, value in yaml.safe_load((root / 'data.yaml').read_text()).items():
+yaml = yaml.YAML(typ='safe', pure=True)
+for key, value in yaml.load((root / 'data.yaml').read_text()).items():
   globals()[key] = value
